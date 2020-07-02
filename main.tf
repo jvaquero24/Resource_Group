370e2609e6ff9d5b8074771bd96b900cf192dd23
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-07-03 00:46:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">=0.12"
-}
-
-provider "azurerm" {
-  version = "=1.29.0"
-
-}
-
-terraform {
-    backend "azurerm" {
-    }
-}
-
 resource "azurerm_resource_group" "RG" {
   name     = var.resource_group_name
   location = var.location
